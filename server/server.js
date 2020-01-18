107b7e1b09d8640e23dc03ccdedc5e89ad1101ef
Sadaf <sadaf_cu@hotmail.com>
2020-01-18 18:32:05 +0500

    fixed start point of server

--- server/server.js
+++ server/server.js
@@ -61,4 +61,14 @@ app.use('/graphql', graphqlHTTP({
 })); 
 ///////////////////////////////
 
+
+app.use(express.static('../client/build'));
+  
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+console.log(path.join(__dirname, '../client/build/index.html'))
+// 
+
 app.listen(port, ()=>console.log(`Server started on port ${port}`))
