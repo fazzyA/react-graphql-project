7319e065c064f5a608b0ef2c9bf623bb70c24986
Sadaf <sadaf_cu@hotmail.com>
2020-03-06 10:31:29 +0500

    delete customer done

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import {
-    MdEdit,
+    
     MdDelete,
   } from 'react-icons/md';
   import { getColor } from 'utils/colors';
 
   import { Link } from 'react-router-dom';
-const ModalExample = (props) => {
+
+  const ModalExample = (props) => {
   const {
     buttonLabel,
     className
@@ -17,17 +18,25 @@ const ModalExample = (props) => {
 
   const toggle = () => setModal(!modal);
 
+  const handleDelete = () => {
+    console.log(props)
+      setModal(false);
+      props.handleDelete(props.deleteRec);
+
+  }
+
   return (
     <>
-      <Button color="danger" onClick={toggle}> <MdDelete size={25}  /> </Button>
+      <Button color="defdault" onClick={toggle} type="link"> <MdDelete size={25} color={getColor('danger')} /> </Button>
      
       <Modal isOpen={modal} toggle={toggle} className={className}>
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
-        <ModalBody>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </ModalBody>
+        <ModalHeader toggle={toggle}>Delete ?</ModalHeader>
+          <ModalBody>
+              Are you sure you want to delete this record?
+              <br></br>
+          </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
+          <Button color="primary" onClick={handleDelete}>Yes</Button>{' '}
           <Button color="secondary" onClick={toggle}>Cancel</Button>
         </ModalFooter>
       </Modal>
